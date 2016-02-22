262e09a75fe0e02c36ef6f64770168ad0948bc5a
btokadabob <okadanob@pm.nttdata.co.jp>
2016-02-22 14:44:37 +0900

    mkdir and move target-project #1

--- first-spring-security/scripts/create-app.sh
+++ first-spring-security/scripts/create-app.sh
@@ -4,6 +4,10 @@
 
 echo "create version is ${VERSION}."
 
+# create dir for complete app
+mkdir target-project
+
+# generate app
 bash ./first-spring-security/scripts/generate-security-project.sh
 
 bash ./todo/scripts/convert-common-infra.sh
@@ -13,3 +17,7 @@ bash ./todo/scripts/convert-common-css.sh
 bash ./first-spring-security/scripts/convert-security-jsp.sh
 
 bash ./first-spring-security/scripts/convert-security-xml.sh
+
+# mv to target-project
+mv ./first-spring-security-mybatis3 ./target-project
+mv ./first-spring-security-mybatis3-multi ./target-project
--- todo-rest/scripts/create-app.sh
+++ todo-rest/scripts/create-app.sh
@@ -4,6 +4,10 @@
 
 echo "create version is ${VERSION}."
 
+# create dir for complete app
+mkdir target-project
+
+# generate app
 bash ./todo-rest/scripts/generate-rest-project.sh
 
 bash ./todo/scripts/convert-common-infra.sh
@@ -13,3 +17,9 @@ bash ./todo/scripts/convert-common-css.sh
 bash ./todo-rest/scripts/convert-rest-msg.sh
 
 bash ./todo-rest/scripts/convert-rest-xml.sh
+
+# mv to target-project
+mv ./todo-jpa-api ./target-project
+mv ./todo-mybatis3-api ./target-project
+mv ./todo-jpa-api-multi ./target-project
+mv ./todo-mybatis3-api-multi ./target-project
