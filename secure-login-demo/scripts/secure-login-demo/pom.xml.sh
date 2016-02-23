ff89a2ad189baf4dec824f5c50e379d8fdaab1d1
btokadabob <okadanob@pm.nttdata.co.jp>
2016-02-23 19:47:44 +0900

    tutorial app init #9

--- secure-login-demo/scripts/secure-login-demo/pom.xml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET_FILE_NAME=secure-login-demo/pom.xml
-SED_FILE_NAME=secure-login-demo/scripts/${TARGET_FILE_NAME}.sed
-
-JAVA_VERSION_LINE=`sed -n '/<java-version>/=' target-project/$TARGET_FILE_NAME`
-sed -i -e "s/__JAVA_VERSION_LINE__/${JAVA_VERSION_LINE}/g" $SED_FILE_NAME
-
-sed -i -f $SED_FILE_NAME target-project/$TARGET_FILE_NAME
