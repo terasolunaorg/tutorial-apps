7ab0ff06deee3920a7ead5c3258fe9d353cc1c03
btokadabob <okadanob@pm.nttdata.co.jp>
2016-02-23 20:00:04 +0900

    todo-rest tutorial #9

---
+++ todo-rest/todo-api-mybatis3-multi/todo-api-mybatis3-multi-domain/src/main/java/todo/domain/service/todo/TodoService.java
@@ -0,0 +1,17 @@
+package todo.domain.service.todo;
+
+import java.util.Collection;
+
+import todo.domain.model.Todo;
+
+public interface TodoService {
+    Collection<Todo> findAll();
+
+    Todo findOne(String todoId);
+
+    Todo create(Todo todo);
+
+    Todo finish(String todoId);
+
+    void delete(String todoId);
+}
