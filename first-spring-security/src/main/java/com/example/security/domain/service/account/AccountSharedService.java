29075b503c6cc6c44e2a9b5fbdbecc8fb774520d
btnoguchimnm <noguchimnm@pm.nttdata.co.jp>
2016-08-31 15:25:52 +0900

    tutorial-apps init

--- first-spring-security/src/main/java/com/example/security/domain/service/account/AccountSharedService.java
+++
@@ -1,8 +0,0 @@
-package com.example.security.domain.service.account;
-
-import com.example.security.domain.model.Account;
-
-public interface AccountSharedService {
-    Account findOne(String username);
-}
-
--- todo/scripts/convert-common-infra.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# -infra.properties
-find . -type f -name '*infra.properties' | xargs sed -i 's/DB_CLOSE_DELAY=-1*/DB_CLOSE_DELAY=-1;INIT=create table if not exists todo(todo_id varchar(36) primary key, todo_title varchar(30), finished boolean, created_at timestamp)/g'
