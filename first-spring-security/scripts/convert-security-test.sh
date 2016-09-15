9e672f9bc29e43a4bcf8e1cf88defe426cac1652
btnoguchimnm <noguchimnm@pm.nttdata.co.jp>
2016-09-15 14:56:40 +0900

    modify script of modify pom.xml.#27

--- first-spring-security/scripts/convert-security-test.sh
+++ first-spring-security/scripts/convert-security-test.sh
@@ -7,6 +7,7 @@ find . -type f -name 'selenium.properties' | xargs sed -i -e 's|selenium.applica
 find ./target-project/first-spring-security-mybatis3-multi/first-spring-security-mybatis3-multi-selenium/src/test/java/com/example/security/selenium/FirstSpringSecurityTest.java | xargs sed -i -e 's|assertThat(driver.findElement(By.tagName("h1"))|assertThat(driver.findElement(By.id("title"))|'
 
 #pom.xml
-if test ${ARCHETYPE_VERSION:0:3} -eq 5.1 ; then
-    find ./target-project/first-spring-security-mybatis3/pom.xml | xargs sed -i -e 's|</dependencies>|<dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
+if test ${ARCHETYPE_VERSION:0:3} = 5.1 ; then
+    find ./target-project/first-spring-security-mybatis3/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
+    find ./target-project/first-spring-security-mybatis3/pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\n    </properties>|'
 fi
