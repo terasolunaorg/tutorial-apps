c8f2b1bf961d073d3d3f8c87f650b3af9b663b5c
btsonyud <sonyud@pm.nttdata.co.jp>
2016-09-28 14:27:19 +0900

    Make a common java sourse. #27

--- todo-rest/scripts/generate-rest-test.sh
+++ todo-rest/scripts/generate-rest-test.sh
@@ -3,28 +3,34 @@
 # make app test
 # jpa
 mkdir -p ./target-project/todo-api-jpa/src/test/java/todo/selenium/todo
+mkdir -p ./target-project/todo-api-jpa/src/test/resources/META-INF/spring
 
 # jpa-multi
 mkdir -p ./target-project/todo-api-jpa-multi/todo-api-jpa-multi-selenium/src/test/java/todo/selenium/todo
 
 # mybatis3
 mkdir -p ./target-project/todo-api-mybatis3/src/test/java/todo/selenium/todo
+mkdir -p ./target-project/todo-api-mybatis3/src/test/resources/META-INF/spring
 
 # mybatis3-multi
 mkdir -p ./target-project/todo-api-mybatis3-multi/todo-api-mybatis3-multi-selenium/src/test/java/todo/selenium/todo
 
 # cp test sources
 # jpa
-cp -p ./todo-rest/src/test/java/jpa/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-jpa/src/test/java/todo/selenium/todo
+cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-jpa/src/test/java/todo/selenium/todo
+cp -p ./todo-rest/src/test/resources/jpa/META-INF/spring/selenium.properties ./target-project/todo-api-jpa/src/test/resources/META-INF/spring
+cp -p ./todo-rest/src/test/resources/jpa/META-INF/spring/seleniumContext.xml ./target-project/todo-api-jpa/src/test/resources/META-INF/spring
 
 # jpa-multi
-cp -p ./todo-rest/src/test/java/jpa-multi/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-jpa-multi/todo-api-jpa-multi-selenium/src/test/java/todo/selenium/todo
+cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-jpa-multi/todo-api-jpa-multi-selenium/src/test/java/todo/selenium/todo
 
 # mybatis3
-cp -p ./todo-rest/src/test/java/mybatis3/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-mybatis3/src/test/java/todo/selenium/todo
+cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-mybatis3/src/test/java/todo/selenium/todo
+cp -p ./todo-rest/src/test/resources/mybatis3/META-INF/spring/selenium.properties ./target-project/todo-api-mybatis3/src/test/resources/META-INF/spring
+cp -p ./todo-rest/src/test/resources/mybatis3/META-INF/spring/seleniumContext.xml ./target-project/todo-api-mybatis3/src/test/resources/META-INF/spring
 
 # mybatis3-multi
-cp -p ./todo-rest/src/test/java/mybatis3-multi/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-mybatis3-multi/todo-api-mybatis3-multi-selenium/src/test/java/todo/selenium/todo/
+cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-mybatis3-multi/todo-api-mybatis3-multi-selenium/src/test/java/todo/selenium/todo/
 
 # modify pom
 # jpa-multi
@@ -43,3 +49,9 @@ find ./target-project/todo-api-jpa/pom.xml | xargs sed -i -e 's|</properties>|  
 find ./target-project/todo-api-mybatis3/pom.xml | xargs sed -i -e 's|<dependencies>|<dependencies>\n    <dependency>\n            <groupId>io.rest-assured</groupId>\n            <artifactId>rest-assured</artifactId>\n            <version>${rest-assured.version}</version>\n            <scope>test</scope>\n        </dependency>|'
 find ./target-project/todo-api-mybatis3/pom.xml | xargs sed -i -e 's|</properties>|    <rest-assured.version>3.0.1</rest-assured.version>\n    </properties>|'
 
+# modify selenium.properties
+# jpa-multi
+echo "selenium.todoApiUrl=\${selenium.applicationContextUrl}/api/v1/todos" >> ./target-project/todo-api-jpa-multi/todo-api-jpa-multi-selenium/src/test/resources/META-INF/spring/selenium.properties
+
+#mybatis3-multi
+echo "selenium.todoApiUrl=\${selenium.applicationContextUrl}/api/v1/todos" >> ./target-project/todo-api-mybatis3-multi/todo-api-mybatis3-multi-selenium/src/test/resources/META-INF/spring/selenium.properties
