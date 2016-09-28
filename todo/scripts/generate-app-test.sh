8a724b9e16fdcf45d8cbeaa20054a2b2316a26ed
btsonyud <sonyud@pm.nttdata.co.jp>
2016-09-28 16:28:33 +0900

    Make a common java sourse. #27

--- todo/scripts/generate-app-test.sh
+++ todo/scripts/generate-app-test.sh
@@ -14,17 +14,17 @@ mkdir -p ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-selenium/src/t
 
 # cp test sources
 # jpa
-cp -p ./todo/src/test/java/jpa/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa/src/test/java/todo/selenium/todo
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa/src/test/java/todo/selenium/todo
 cp -p ./todo/src/test/resources/jpa/META-INF/spring/selenium.properties ./target-project/todo-jpa/src/test/resources/META-INF/spring
 cp -p ./todo/src/test/resources/jpa/META-INF/spring/seleniumContext.xml ./target-project/todo-jpa/src/test/resources/META-INF/spring
 # jpa-multi
-cp -p ./todo/src/test/java/jpa-multi/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa-multi/todo-jpa-multi-selenium/src/test/java/todo/selenium/todo
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa-multi/todo-jpa-multi-selenium/src/test/java/todo/selenium/todo
 # mybatis3
-cp -p ./todo/src/test/java/mybatis3/todo/selenium/todo/TodoTest.java ./target-project/todo-mybatis3/src/test/java/todo/selenium/todo
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-mybatis3/src/test/java/todo/selenium/todo
 cp -p ./todo/src/test/resources/mybatis3/META-INF/spring/selenium.properties ./target-project/todo-mybatis3/src/test/resources/META-INF/spring
 cp -p ./todo/src/test/resources/mybatis3/META-INF/spring/seleniumContext.xml ./target-project/todo-mybatis3/src/test/resources/META-INF/spring
 # mybatis3-multi
-cp -p ./todo/src/test/java/mybatis3-multi/todo/selenium/todo/TodoTest.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-selenium/src/test/java/todo/selenium/todo
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-selenium/src/test/java/todo/selenium/todo
 
 VER="${ARCHETYPE_VERSION:0:3}"
 
