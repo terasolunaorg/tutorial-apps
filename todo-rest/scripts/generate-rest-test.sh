b78306ad20794fa6b521a731fd7a9edd7a6e20dc
btsonyud <sonyud@pm.nttdata.co.jp>
2016-09-29 17:38:09 +0900

    Add todo-api test. #27

--- todo-rest/scripts/generate-rest-test.sh
+++ todo-rest/scripts/generate-rest-test.sh
@@ -15,6 +15,10 @@ mkdir -p ./target-project/todo-api-mybatis3/src/test/resources/META-INF/spring
 # mybatis3-multi
 mkdir -p ./target-project/todo-api-mybatis3-multi/todo-api-mybatis3-multi-selenium/src/test/java/todo/selenium/todo
 
+# map
+mkdir -p ./target-project/todo-api/src/test/java/todo/selenium/todo
+mkdir -p ./target-project/todo-api/src/test/resources/META-INF/spring
+
 # cp test sources
 # jpa
 cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-jpa/src/test/java/todo/selenium/todo
@@ -32,6 +36,11 @@ cp -p ./todo-rest/src/test/resources/mybatis3/META-INF/spring/seleniumContext.xm
 # mybatis3-multi
 cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-mybatis3-multi/todo-api-mybatis3-multi-selenium/src/test/java/todo/selenium/todo/
 
+# map
+cp -p ./todo-rest/src/test/java/todo/selenium/todo/TodoApiTest.java ./target-project/todo-api-mybatis3/src/test/java/todo/selenium/todo
+cp -p ./todo-rest/src/test/resources/map/META-INF/spring/selenium.properties ./target-project/todo-api/src/test/resources/META-INF/spring
+cp -p ./todo-rest/src/test/resources/map/META-INF/spring/seleniumContext.xml ./target-project/todo-api/src/test/resources/META-INF/spring
+
 # modify pom
 # jpa-multi
 find ./target-project/todo-api-jpa-multi/todo-api-jpa-multi-selenium/pom.xml | xargs sed -i -e 's|<dependencies>|<dependencies>\n    <dependency>\n            <groupId>io.rest-assured</groupId>\n            <artifactId>rest-assured</artifactId>\n            <version>${rest-assured.version}</version>\n            <scope>test</scope>\n        </dependency>|'
@@ -48,6 +57,10 @@ find ./target-project/todo-api-jpa/pom.xml | xargs sed -i -e 's|</properties>|  
 # mybatis3
 find ./target-project/todo-api-mybatis3/pom.xml | xargs sed -i -e 's|<dependencies>|<dependencies>\n    <dependency>\n            <groupId>io.rest-assured</groupId>\n            <artifactId>rest-assured</artifactId>\n            <version>${rest-assured.version}</version>\n            <scope>test</scope>\n        </dependency>|'
 find ./target-project/todo-api-mybatis3/pom.xml | xargs sed -i -e 's|</properties>|    <rest-assured.version>3.0.1</rest-assured.version>\n    </properties>|'
+
+# map
+find ./target-project/todo-api/pom.xml | xargs sed -i -e 's|<dependencies>|<dependencies>\n    <dependency>\n            <groupId>io.rest-assured</groupId>\n            <artifactId>rest-assured</artifactId>\n            <version>${rest-assured.version}</version>\n            <scope>test</scope>\n        </dependency>|'
+find ./target-project/todo-api/pom.xml | xargs sed -i -e 's|</properties>|    <rest-assured.version>3.0.1</rest-assured.version>\n    </properties>|'
 
 # modify selenium.properties
 # jpa-multi
