751b4d26a8ab1ba1a5a339a1370bf01bbccfb7b1
btsonyud <sonyud@pm.nttdata.co.jp>
2016-09-30 16:54:17 +0900

    non db>mybatis>jpa. #27

--- todo/scripts/generate-app-project.sh
+++ todo/scripts/generate-app-project.sh
@@ -4,7 +4,20 @@
 #${VERSION}=tutorial pj version
 
 
-# mybatis single, mybatis multi, jpa single, jpa multi, map single
+# single without any DB configuration, mybatis single, mybatis multi, jpa single, jpa multi
+# create dir for complete map single
+mkdir tmp
+cd ./tmp
+# single without any DB configuration
+mvn archetype:generate -B \
+ -DarchetypeCatalog=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-releases \
+ -DarchetypeGroupId=org.terasoluna.gfw.blank \
+ -DarchetypeArtifactId=terasoluna-gfw-web-blank-archetype \
+ -DarchetypeVersion=${ARCHETYPE_VERSION} \
+ -DgroupId=todo \
+ -DartifactId=todo \
+ -Dversion=${VERSION}
+cd ..
 # mybatis app single
 mvn archetype:generate -B \
  -DarchetypeCatalog=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-releases \
@@ -45,22 +58,6 @@ mvn archetype:generate -B \
  -DartifactId=todo-jpa-multi \
  -Dversion=${VERSION}
 
-# create dir for complete map single
-mkdir tmp
-cd ./tmp
-
-# map single
-mvn archetype:generate -B \
- -DarchetypeCatalog=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-releases \
- -DarchetypeGroupId=org.terasoluna.gfw.blank \
- -DarchetypeArtifactId=terasoluna-gfw-web-blank-archetype \
- -DarchetypeVersion=${ARCHETYPE_VERSION} \
- -DgroupId=todo \
- -DartifactId=todo \
- -Dversion=${VERSION}
-
-cd ..
-
 # make app
 mkdir -p ./todo-mybatis3/src/main/java/todo/app/todo
 mkdir -p ./todo-mybatis3-multi/todo-mybatis3-multi-web/src/main/java/todo/app/todo
@@ -70,80 +67,83 @@ mkdir -p ./tmp/todo/src/main/java/todo/app/todo
 
 
 # repository
+mkdir -p ./tmp/todo/src/main/java/todo/domain/repository/todo
 mkdir -p ./todo-mybatis3/src/main/java/todo/domain/repository/todo
 mkdir -p ./todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/java/todo/domain/repository/todo
 mkdir -p ./todo-jpa/src/main/java/todo/domain/repository/todo
 mkdir -p ./todo-jpa-multi/todo-jpa-multi-domain/src/main/java/todo/domain/repository/todo
-mkdir -p ./tmp/todo/src/main/java/todo/domain/repository/todo
 
 # repository for xml
 mkdir -p ./todo-mybatis3/src/main/resources/todo/domain/repository/todo
 mkdir -p ./todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/resources/todo/domain/repository/todo
 
 #service
+mkdir -p ./tmp/todo/src/main/java/todo/domain/service/todo
 mkdir -p ./todo-mybatis3/src/main/java/todo/domain/service/todo
 mkdir -p ./todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/java/todo/domain/service/todo
 mkdir -p ./todo-jpa/src/main/java/todo/domain/service/todo
 mkdir -p ./todo-jpa-multi/todo-jpa-multi-domain/src/main/java/todo/domain/service/todo
-mkdir -p ./tmp/todo/src/main/java/todo/domain/service/todo
 
 # app
+mkdir -p ./tmp/todo/src/main/webapp/WEB-INF/views/todo/
 mkdir -p ./todo-mybatis3/src/main/webapp/WEB-INF/views/todo/
 mkdir -p ./todo-mybatis3-multi/todo-mybatis3-multi-web/src/main/webapp/WEB-INF/views/todo/
 mkdir -p ./todo-jpa/src/main/webapp/WEB-INF/views/todo/
 mkdir -p ./todo-jpa-multi/todo-jpa-multi-web/src/main/webapp/WEB-INF/views/todo/
-mkdir -p ./tmp/todo/src/main/webapp/WEB-INF/views/todo/
 
 # create dir for complete app
 mkdir target-project
 
 #mv to target-project
-mv ./todo-jpa ./target-project
-mv ./todo-mybatis3 ./target-project
-mv ./todo-jpa-multi ./target-project
-mv ./todo-mybatis3-multi ./target-project
 mv ./tmp/todo ./target-project
+mv ./todo-mybatis3 ./target-project
+mv ./todo-mybatis3-multi ./target-project
+mv ./todo-jpa ./target-project
+mv ./todo-jpa-multi ./target-project
 
 rm -r ./tmp
 
 # cp common sources
+cp -p ./todo/src/main/java/todo/app/todo/TodoForm.java ./target-project/todo/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoForm.java ./target-project/todo-mybatis3/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoForm.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-web/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoForm.java ./target-project/todo-jpa/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoForm.java ./target-project/todo-jpa-multi/todo-jpa-multi-web/src/main/java/todo/app/todo
-cp -p ./todo/src/main/java/todo/app/todo/TodoForm.java ./target-project/todo/src/main/java/todo/app/todo
 
+cp -p ./todo/src/main/java/todo/app/todo/TodoController.java ./target-project/todo/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoController.java ./target-project/todo-mybatis3/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoController.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-web/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoController.java ./target-project/todo-jpa/src/main/java/todo/app/todo
 cp -p ./todo/src/main/java/todo/app/todo/TodoController.java ./target-project/todo-jpa-multi/todo-jpa-multi-web/src/main/java/todo/app/todo
-cp -p ./todo/src/main/java/todo/app/todo/TodoController.java ./target-project/todo/src/main/java/todo/app/todo
 
 # model
+# single without any DB configuration
+cp -p ./todo/src/main/java/mybatis3/todo/domain/model/Todo.java ./target-project/todo/src/main/java/todo/domain/model/
 # mybatis3
 cp -p ./todo/src/main/java/mybatis3/todo/domain/model/Todo.java ./target-project/todo-mybatis3/src/main/java/todo/domain/model/
 cp -p ./todo/src/main/java/mybatis3/todo/domain/model/Todo.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/java/todo/domain/model/
 # jpa
 cp -p ./todo/src/main/java/jpa/todo/domain/model/Todo.java ./target-project/todo-jpa/src/main/java/todo/domain/model/
 cp -p ./todo/src/main/java/jpa/todo/domain/model/Todo.java ./target-project/todo-jpa-multi/todo-jpa-multi-domain/src/main/java/todo/domain/model/
-# map
-cp -p ./todo/src/main/java/mybatis3/todo/domain/model/Todo.java ./target-project/todo/src/main/java/todo/domain/model/
 
 # service
 # TodoService
+cp -p ./todo/src/main/java/todo/domain/service/todo/TodoService.java ./target-project/todo/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/todo/domain/service/todo/TodoService.java ./target-project/todo-mybatis3/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/todo/domain/service/todo/TodoService.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/todo/domain/service/todo/TodoService.java ./target-project/todo-jpa/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/todo/domain/service/todo/TodoService.java ./target-project/todo-jpa-multi/todo-jpa-multi-domain/src/main/java/todo/domain/service/todo/
-cp -p ./todo/src/main/java/todo/domain/service/todo/TodoService.java ./target-project/todo/src/main/java/todo/domain/service/todo/
 # TodoServiceImpl
+cp -p ./todo/src/main/java/mybatis3/todo/domain/service/todo/TodoServiceImpl.java ./target-project/todo/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/mybatis3/todo/domain/service/todo/TodoServiceImpl.java ./target-project/todo-mybatis3/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/mybatis3/todo/domain/service/todo/TodoServiceImpl.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/jpa/todo/domain/service/todo/TodoServiceImpl.java ./target-project/todo-jpa/src/main/java/todo/domain/service/todo/
 cp -p ./todo/src/main/java/jpa/todo/domain/service/todo/TodoServiceImpl.java ./target-project/todo-jpa-multi/todo-jpa-multi-domain/src/main/java/todo/domain/service/todo/
-cp -p ./todo/src/main/java/mybatis3/todo/domain/service/todo/TodoServiceImpl.java ./target-project/todo/src/main/java/todo/domain/service/todo/
 
 # repository
+# single without any DB configuration
+cp -p ./todo/src/main/java/mybatis3/todo/domain/repository/todo/TodoRepository.java ./target-project/todo/src/main/java/todo/domain/repository/todo/
+cp -p ./todo/src/main/java/todo/domain/repository/todo/TodoRepositoryImpl.java ./target-project/todo/src/main/java/todo/domain/repository/todo/
 # mybatis3
 cp -p ./todo/src/main/java/mybatis3/todo/domain/repository/todo/TodoRepository.java ./target-project/todo-mybatis3/src/main/java/todo/domain/repository/todo/
 cp -p ./todo/src/main/java/mybatis3/todo/domain/repository/todo/TodoRepository.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/java/todo/domain/repository/todo/
@@ -153,13 +153,10 @@ cp -p ./todo/src/main/java/jpa/todo/domain/repository/todo/TodoRepository.java .
 # xml
 cp -p ./todo/src/main/resources/todo/domain/repository/todo/TodoRepository.xml ./target-project/todo-mybatis3/src/main/resources/todo/domain/repository/todo/
 cp -p ./todo/src/main/resources/todo/domain/repository/todo/TodoRepository.xml ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-domain/src/main/resources/todo/domain/repository/todo/
-# map
-cp -p ./todo/src/main/java/mybatis3/todo/domain/repository/todo/TodoRepository.java ./target-project/todo/src/main/java/todo/domain/repository/todo/
-cp -p ./todo/src/main/java/todo/domain/repository/todo/TodoRepositoryImpl.java ./target-project/todo/src/main/java/todo/domain/repository/todo/
 
 # webapp
+cp -p ./todo/src/main/webapp/WEB-INF/views/todo/list.jsp ./target-project/todo/src/main/webapp/WEB-INF/views/todo/
 cp -p ./todo/src/main/webapp/WEB-INF/views/todo/list.jsp ./target-project/todo-mybatis3/src/main/webapp/WEB-INF/views/todo/
 cp -p ./todo/src/main/webapp/WEB-INF/views/todo/list.jsp ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-web/src/main/webapp/WEB-INF/views/todo/
 cp -p ./todo/src/main/webapp/WEB-INF/views/todo/list.jsp ./target-project/todo-jpa/src/main/webapp/WEB-INF/views/todo/
 cp -p ./todo/src/main/webapp/WEB-INF/views/todo/list.jsp ./target-project/todo-jpa-multi/todo-jpa-multi-web/src/main/webapp/WEB-INF/views/todo/
-cp -p ./todo/src/main/webapp/WEB-INF/views/todo/list.jsp ./target-project/todo/src/main/webapp/WEB-INF/views/todo/
--- todo/scripts/generate-app-test.sh
+++ todo/scripts/generate-app-test.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
 # make app test
-# jpa
-mkdir -p ./target-project/todo-jpa/src/test/java/todo/selenium/todo
-mkdir -p ./target-project/todo-jpa/src/test/resources/META-INF/spring
-# jpa-multi
-mkdir -p ./target-project/todo-jpa-multi/todo-jpa-multi-selenium/src/test/java/todo/selenium/todo
+# single without any DB configuration
+mkdir -p ./target-project/todo/src/test/java/todo/selenium/todo
+mkdir -p ./target-project/todo/src/test/resources/META-INF/spring
 # mybatis3
 mkdir -p ./target-project/todo-mybatis3/src/test/java/todo/selenium/todo
 mkdir -p ./target-project/todo-mybatis3/src/test/resources/META-INF/spring
 # mybatis3-multi
 mkdir -p ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-selenium/src/test/java/todo/selenium/todo
-# map
-mkdir -p ./target-project/todo/src/test/java/todo/selenium/todo
-mkdir -p ./target-project/todo/src/test/resources/META-INF/spring
+# jpa
+mkdir -p ./target-project/todo-jpa/src/test/java/todo/selenium/todo
+mkdir -p ./target-project/todo-jpa/src/test/resources/META-INF/spring
+# jpa-multi
+mkdir -p ./target-project/todo-jpa-multi/todo-jpa-multi-selenium/src/test/java/todo/selenium/todo
 
 # cp test sources
-# jpa
-cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa/src/test/java/todo/selenium/todo
-cp -p ./todo/src/test/resources/jpa/META-INF/spring/selenium.properties ./target-project/todo-jpa/src/test/resources/META-INF/spring
-cp -p ./todo/src/test/resources/jpa/META-INF/spring/seleniumContext.xml ./target-project/todo-jpa/src/test/resources/META-INF/spring
-# jpa-multi
-cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa-multi/todo-jpa-multi-selenium/src/test/java/todo/selenium/todo
+# single without any DB configuration
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo/src/test/java/todo/selenium/todo
+cp -p ./todo/src/test/resources/map/META-INF/spring/selenium.properties ./target-project/todo/src/test/resources/META-INF/spring
+cp -p ./todo/src/test/resources/map/META-INF/spring/seleniumContext.xml ./target-project/todo/src/test/resources/META-INF/spring
 # mybatis3
 cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-mybatis3/src/test/java/todo/selenium/todo
 cp -p ./todo/src/test/resources/mybatis3/META-INF/spring/selenium.properties ./target-project/todo-mybatis3/src/test/resources/META-INF/spring
 cp -p ./todo/src/test/resources/mybatis3/META-INF/spring/seleniumContext.xml ./target-project/todo-mybatis3/src/test/resources/META-INF/spring
 # mybatis3-multi
 cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-mybatis3-multi/todo-mybatis3-multi-selenium/src/test/java/todo/selenium/todo
-# map
-cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo/src/test/java/todo/selenium/todo
-cp -p ./todo/src/test/resources/map/META-INF/spring/selenium.properties ./target-project/todo/src/test/resources/META-INF/spring
-cp -p ./todo/src/test/resources/map/META-INF/spring/seleniumContext.xml ./target-project/todo/src/test/resources/META-INF/spring
+# jpa
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa/src/test/java/todo/selenium/todo
+cp -p ./todo/src/test/resources/jpa/META-INF/spring/selenium.properties ./target-project/todo-jpa/src/test/resources/META-INF/spring
+cp -p ./todo/src/test/resources/jpa/META-INF/spring/seleniumContext.xml ./target-project/todo-jpa/src/test/resources/META-INF/spring
+# jpa-multi
+cp -p ./todo/src/test/java/todo/selenium/todo/TodoTest.java ./target-project/todo-jpa-multi/todo-jpa-multi-selenium/src/test/java/todo/selenium/todo
 
 VER="${ARCHETYPE_VERSION:0:3}"
 
 # modify pom
 # VER 1.0.x or 5.0.x or 5.1.x
 if test ${VER} = 1.0 ||  test ${VER} = 5.0 || test ${VER} = 5.1 ; then
-# jpa
-find ./target-project/todo-jpa/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
-find ./target-project/todo-jpa/pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\n    </properties>|'
+# single without any DB configuration
+find ./target-project/todo/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
+find ./target-project/todo/pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\n    </properties>|'
 # mybatis3
 find ./target-project/todo-mybatis3/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
 find ./target-project/todo-mybatis3/pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\n    </properties>|'
-# map
-find ./target-project/todo/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
-find ./target-project/todo/pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\n    </properties>|'
+# jpa
+find ./target-project/todo-jpa/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\n            <groupId>org.seleniumhq.selenium</groupId>\n            <artifactId>selenium-java</artifactId>\n            <version>${selenium.version}</version>\n            <scope>test</scope>\n        </dependency>\n    </dependencies>|'
+find ./target-project/todo-jpa/pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\n    </properties>|'
 fi
