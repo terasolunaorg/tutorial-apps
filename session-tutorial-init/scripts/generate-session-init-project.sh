669f6c765cd396d69c5fafcd0d173284fe40efd2
btnoguchimnm <noguchimnm@pm.nttdata.co.jp>
2016-11-18 15:33:46 +0900

    Corresponds to the archetype of x.x.x-SNAPSHOT for session-tutorial-init.#27

--- session-tutorial-init/scripts/generate-session-init-project.sh
+++ session-tutorial-init/scripts/generate-session-init-project.sh
@@ -3,11 +3,18 @@
 #${ARCHETYPE_VERSION}=terasoluna-gfw-blank-archetype-version
 #${VERSION}=tutorial pj version
 
+# settings of maven archetype catalog
+if test `echo ${ARCHETYPE_VERSION} | tail -c8 ` = "RELEASE"; then
+ ARCHETYPE_CATALOG=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-releases
+else
+ ARCHETYPE_CATALOG=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-snapshots
+fi
+
 cd target-project
 
 # mybatis app multi
 mvn archetype:generate -B \
- -DarchetypeCatalog=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-releases \
+ -DarchetypeCatalog=${ARCHETYPE_CATALOG} \
  -DarchetypeGroupId=org.terasoluna.gfw.blank \
  -DarchetypeArtifactId=terasoluna-gfw-multi-web-blank-mybatis3-archetype \
  -DarchetypeVersion=${ARCHETYPE_VERSION} \
