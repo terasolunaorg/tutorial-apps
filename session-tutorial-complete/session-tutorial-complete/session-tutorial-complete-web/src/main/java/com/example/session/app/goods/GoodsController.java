088cb5c9a2dea8cdb3e2f796e247f518559d84a8
btkobayashirun <kobayashirun@pm.nttdata.co.jp>
2017-12-14 18:32:49 +0900

    session tutorial

---
+++ session-tutorial-complete/session-tutorial-complete/session-tutorial-complete-web/src/main/java/com/example/session/app/goods/GoodsController.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2013-2017 NTT DATA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.session.app.goods;
+ 
+import javax.inject.Inject;
+ 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.terasoluna.gfw.common.message.ResultMessages;
+ 
+import com.example.session.domain.model.Cart;
+import com.example.session.domain.model.CartItem;
+import com.example.session.domain.model.Goods;
+import com.example.session.domain.service.goods.GoodsService;
+ 
+@Controller
+@RequestMapping("goods")
+public class GoodsController {
+ 
+	@Inject
+	GoodsService goodsService;
+
+    
+    @Inject
+    Cart cart;
+
+    // (1)
+    @Inject
+    GoodsSearchCriteria criteria;
+ 
+	@ModelAttribute(value = "goodViewForm")
+	public GoodViewForm setUpCategoryId() {
+		return new GoodViewForm();
+	}
+ 
+	// (2)
+    @RequestMapping(value = "", method = RequestMethod.GET)
+	String showGoods(GoodViewForm form, Model model) {
+        Pageable pageable = new PageRequest(criteria.getPage(), 3);
+        form.setCategoryId(criteria.getCategoryId());
+        return showGoods(pageable, model);
+    }
+
+    // (3)
+    @RequestMapping(value = "", method = RequestMethod.GET, params = "categoryId")
+    String changeCategoryId(GoodViewForm form, Pageable pageable, Model model) {
+        criteria.setPage(pageable.getPageNumber());
+        criteria.setCategoryId(form.getCategoryId());
+        return showGoods(pageable, model);
+    }
+
+    // (4)
+    @RequestMapping(value = "", method = RequestMethod.GET, params = "page")
+    String changePage(GoodViewForm form, Pageable pageable, Model model) {
+        criteria.setPage(pageable.getPageNumber());
+        form.setCategoryId(criteria.getCategoryId());
+        return showGoods(pageable, model);
+    }
+
+    // (5)
+    String showGoods(Pageable pageable, Model model) {
+ 
+		Page<Goods> page = goodsService.findByCategoryId(
+				criteria.getCategoryId(), pageable);
+		model.addAttribute("page", page);
+		return "goods/showGoods";
+	}
+ 
+	@RequestMapping(value = "/{goodsId}", method = RequestMethod.GET)
+	public String showGoodsDetail(@PathVariable String goodsId, Model model) {
+ 
+		Goods goods = goodsService.findOne(goodsId);
+		model.addAttribute(goods);
+ 
+		return "/goods/showGoodsDetail";
+    }
+
+    @RequestMapping(value = "/addToCart", method = RequestMethod.POST)
+    public String addToCart(@Validated GoodAddForm form, BindingResult result,
+            RedirectAttributes attributes) {
+
+        if (result.hasErrors()) {
+            ResultMessages messages = ResultMessages.error()
+                    .add("e.st.go.5001");
+            attributes.addFlashAttribute(messages);
+            return "redirect:/goods";
+        }
+
+        Goods goods = goodsService.findOne(form.getGoodsId());
+        CartItem cartItem = new CartItem();
+        cartItem.setGoods(goods);
+        cartItem.setQuantity(form.getQuantity());
+        cart.add(cartItem);
+
+        return "redirect:/goods";
+	}
+}
---
+++ session-tutorial-complete/session-tutorial-complete/session-tutorial-complete-web/src/main/webapp/WEB-INF/views/account/updateForm1.jsp
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Account Update Page</title>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/app/css/styles.css">
+</head>
+<body>
+
+    <div class="container">
+        <%-- (1) --%>
+        <form:form action="${pageContext.request.contextPath}/account/update"
+            method="post" modelAttribute="accountUpdateForm">
+
+            <h2>Account Update Page 1/2</h2>
+            <table>
+                <tr>
+                    <td><form:label path="name" cssErrorClass="error-label">name</form:label></td>
+                    <%-- (2) --%>
+                    <td><form:input path="name" cssErrorClass="error-input" /> <form:errors
+                            path="name" cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <td><form:label path="email" cssErrorClass="error-label">e-mail</form:label></td>
+                    <td><form:input path="email" cssErrorClass="error-input" /> <form:errors
+                            path="email" cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <td><form:label path="birthday" cssErrorClass="error-label">birthday</form:label></td>
+                    <td><fmt:formatDate value="${accountUpdateForm.birthday}"
+                            pattern="yyyy-MM-dd" var="formattedBirthday" /> <input
+                        type="date" id="birthday" name="birthday"
+                        value="${formattedBirthday}"> <form:errors path="birthday"
+                            cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <td><form:label path="zip" cssErrorClass="error-label">zip</form:label></td>
+                    <td><form:input path="zip" cssErrorClass="error-input" /> <form:errors
+                            path="zip" cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <td><form:label path="address" cssErrorClass="error-label">address</form:label></td>
+                    <td><form:input path="address" cssErrorClass="error-input" />
+                        <form:errors path="address" cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><input type="submit" name="form2" id="next" value="next" /></td>
+                </tr>
+            </table>
+        </form:form>
+
+        <form method="get"
+            action="${pageContext.request.contextPath}/account/update">
+            <input type="submit" name="home" id="home" value="home" />
+        </form>
+    </div>
+</body>
+</html>
