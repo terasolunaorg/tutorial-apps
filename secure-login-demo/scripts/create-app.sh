2b7e4da7f8719648286d97b54bd2ace10d06ae1c
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2018-02-26 14:46:55 +0900

    fix secure-login #215
    (cherry picked from commit 5fd0df931b6e0e04dc68eaad8a09cf46979d5d76)

--- secure-login-demo/scripts/create-app.sh
+++ secure-login-demo/scripts/create-app.sh
@@ -3,6 +3,7 @@
 # Required variables:
 #   ${ARCHETYPE_ARTIFACT_ID}=Artifact ID of blank project's archetype.
 #   ${ARCHETYPE_VERSION}=Version of blank project's archetype. If not specified, the default value is set.
+#   ${ARTIFACT_ID}=Artifact ID of tutorial project. If not specified, the default value is set.
 #   ${VERSION}=Version of tutorial project. If not specified, the default value is set.
 #   ${HOST_IP}=IP address for access to this web application. Defaults to 'localhost'.
 #   ${APSRV_WEB_PORT}=Port number for access to this web application. Defaults to '8080'.
@@ -11,7 +12,7 @@
 case "$ARCHETYPE_ARTIFACT_ID" in
     terasoluna-gfw-multi-web-blank-mybatis3-archetype )
         export GROUP_ID=com.example.securelogin
-        export ARTIFACT_ID=secure-login
+        export ARTIFACT_ID=${ARTIFACT_ID:=secure-login}
         ;;
     * )
         echo "[ERROR] You can not set ${ARCHETYPE_ARTIFACT_ID} for ARCHETYPE_ARTIFACT_ID."
@@ -21,10 +22,16 @@ esac
 
 export ARCHETYPE_VERSION=${ARCHETYPE_VERSION:=5.5.0-SNAPSHOT}
 export VERSION=${VERSION:=1.0.0-SNAPSHOT}
+export HOST_IP=${HOST_IP:=localhost}
+export APSRV_WEB_PORT=${APSRV_WEB_PORT:=8080}
+export APSRV_H2DB_PORT=${APSRV_H2DB_PORT:=9212}
 
 echo "create groupId is ${GROUP_ID}."
 echo "create artifactId is ${ARTIFACT_ID}."
 echo "create version is ${VERSION}."
+echo "IP address for this web application is ${HOST_IP}"
+echo "port number for this web application is ${APSRV_WEB_PORT}"
+echo "port number for H2DB is ${APSRV_H2DB_PORT}"
 
 SCRIPT_DIR=`dirname "$0"`
 TARGET_DIR=${SCRIPT_DIR}/../target-project
@@ -33,18 +40,6 @@ TARGET_DIR=${SCRIPT_DIR}/../target-project
 rm -rf "${TARGET_DIR}/${ARTIFACT_ID}"
 mkdir "$TARGET_DIR"
 pushd "$TARGET_DIR"
-
-if test -z $HOST_IP; then
-  export HOST_IP=localhost
-fi
-
-if test -z $APSRV_WEB_PORT; then
-  export APSRV_WEB_PORT=8080
-fi
-
-if test -z $APSRV_H2DB_PORT; then
-  export APSRV_H2DB_PORT=9212
-fi
 
 bash ../../common/scripts/generate-project.sh
 
