c60387989f10ad3ecf5a83e2b7537b0b33d5670c
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2018-03-08 16:43:22 +0900

    change connection setting to the DB used by the application is defined in env, not selenium #251

--- secure-login-demo/scripts/convert-secure-login-infra.sh
+++ secure-login-demo/scripts/convert-secure-login-infra.sh
@@ -4,6 +4,7 @@
 #   ${ARTIFACT_ID}=Artifact ID of tutorial project.
 #   ${HOST_IP}=IP address for access to this web application.
 #   ${APSRV_WEB_PORT}=Port number for access to this web application.
+#   ${APSRV_H2DB_PORT}=Port number for access to H2DB.
 # Parameters:
 #   $1 : (Optional) Target project path to convert.
 
@@ -14,7 +15,9 @@ fi
 
 # env/-infra.properties
 ENV_INFRA_PROPERTIES=`find ./${ARTIFACT_ID}-env -type f -name "${ARTIFACT_ID}-infra.properties"`
-for i in ${ENV_INFRA_PROPERTIES} ; do echo -e " 
+for i in ${ENV_INFRA_PROPERTIES} ; do echo -e "database.host = ${HOST_IP}
+database.port = ${APSRV_H2DB_PORT}
+
 mail.host=localhost
 mail.port=3025
 mail.from=info@example.com
