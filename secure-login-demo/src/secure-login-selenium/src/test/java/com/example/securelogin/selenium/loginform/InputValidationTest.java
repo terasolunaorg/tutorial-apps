cd84b266fc22aded05d39cb3562587cdc9795394
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2018-03-13 13:37:23 +0900

    use try-with-resources #225

--- secure-login-demo/src/secure-login-selenium/src/test/java/com/example/securelogin/selenium/loginform/InputValidationTest.java
+++ secure-login-demo/src/secure-login-selenium/src/test/java/com/example/securelogin/selenium/loginform/InputValidationTest.java
@@ -51,14 +51,8 @@ public class InputValidationTest extends DBLogFunctionTestSupport {
 
     @Before
     public void setup() throws ScriptException, SQLException {
-        Connection connection = null;
-        try {
-            connection = dataSource.getConnection();
+        try (Connection connection = dataSource.getConnection()) {
             populator.populate(connection);
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
     }
 
