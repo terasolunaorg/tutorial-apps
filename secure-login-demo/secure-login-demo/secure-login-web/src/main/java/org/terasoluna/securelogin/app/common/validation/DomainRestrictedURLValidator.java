c0b12d63b6bac7f5ad74d99be07d8d138135eba4
btkatoutmj <katoutmj@pm.nttdata.co.jp>
2018-03-15 16:06:06 +0900

    secure-login-demo

---
+++ secure-login-demo/secure-login-demo/secure-login-web/src/main/java/org/terasoluna/securelogin/app/common/validation/DomainRestrictedURLValidator.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2013-2018 NTT DATA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.terasoluna.securelogin.app.common.validation;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.util.StringUtils;
+
+public class DomainRestrictedURLValidator implements
+		ConstraintValidator<DomainRestrictedURL, CharSequence> {
+
+	private static final Pattern URL_REGEX = Pattern
+			.compile("(?i)^(?:[a-z](?:[-a-z0-9\\+\\.])*)" + // protocol
+					":(?:\\/\\/([^\\/:]+)" + // auth+host/ip
+					"(?::([0-9]*))?" + // port
+					"(?:\\/.*)*)$");
+
+	private Set<String> allowedDomains;
+
+	@Override
+	public void initialize(DomainRestrictedURL constraintAnnotation) {
+		allowedDomains = new HashSet<String>(Arrays.asList(constraintAnnotation
+				.allowedDomains()));
+	}
+
+	@Override
+	public boolean isValid(CharSequence value,
+			ConstraintValidatorContext context) {
+		Matcher urlMatcher = URL_REGEX.matcher(value);
+		if (urlMatcher.matches()) {
+			String host = urlMatcher.group(1);
+			for (String domain : allowedDomains) {
+				if (StringUtils.hasLength(host) && host.endsWith("." + domain)) {
+					return true;
+				}
+			}
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
---
+++ secure-login-demo/secure-login-demo/secure-login-web/src/main/java/org/terasoluna/securelogin/app/common/validation/FileExtensionValidator.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2013-2018 NTT DATA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.terasoluna.securelogin.app.common.validation;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileExtensionValidator implements
+		ConstraintValidator<FileExtension, MultipartFile> {
+
+	private Set<String> extensions;
+
+	private boolean ignoreCase;
+
+	@Override
+	public void initialize(FileExtension constraintAnnotation) {
+		this.extensions = new HashSet<String>(
+				Arrays.asList(constraintAnnotation.extensions()));
+		this.ignoreCase = constraintAnnotation.ignoreCase();
+	}
+
+	@Override
+	public boolean isValid(MultipartFile value,
+			ConstraintValidatorContext context) {
+		if (value == null) {
+			return true;
+		}
+
+		String fileNameExtension = StringUtils.getFilenameExtension(value
+				.getOriginalFilename());
+		if (!StringUtils.hasLength(fileNameExtension)) {
+			return false;
+		}
+
+		for (String extension : extensions) {
+			if (fileNameExtension.equals(extension) || ignoreCase
+					&& fileNameExtension.equalsIgnoreCase(extension)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
