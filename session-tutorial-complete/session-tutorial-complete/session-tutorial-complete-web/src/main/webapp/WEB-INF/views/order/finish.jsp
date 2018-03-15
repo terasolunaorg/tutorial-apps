067000ef137041ef8d61975216afa7b5598b98c3
btkatoutmj <katoutmj@pm.nttdata.co.jp>
2018-03-15 16:34:44 +0900

    session tutorial

---
+++ session-tutorial-complete/session-tutorial-complete/session-tutorial-complete-web/src/main/webapp/WEB-INF/views/order/finish.jsp
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Order Page</title>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/app/css/styles.css">
+</head>
+<body>
+
+    <sec:authentication property="principal" var="userDetails" />
+
+    <div style="display: inline-flex">
+        welcome ${f:h(userDetails.account.name)}
+        <form:form method="post"
+            action="${pageContext.request.contextPath}/logout">
+            <input type="submit" id="logout" value="logout" />
+        </form:form>
+        <form method="get"
+            action="${pageContext.request.contextPath}/account/update">
+            <input type="submit" name="form1" id="updateAccount"
+                value="Account Update" />
+        </form>
+    </div>
+    <br>
+    <br>
+
+    <div>
+
+        <h3>Your order has been accepted</h3>
+        <table>
+            <tr>
+                <td><label for="orderNumber">order number</label></td>
+                <td><span id="orderNumber">${f:h(order.id)}</span></td>
+            </tr>
+            <tr>
+                <td><label for="orderDate">order date</label></td>
+                <td><span id="orderDate"><fmt:formatDate
+                            value="${order.orderDate}" pattern="yyyy-MM-dd　hh:mm:ss" /></span></td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+            </tr>
+            <c:forEach items="${order.orderLines}" var="orderLine" varStatus="status">
+                <tr>
+                    <td><span id="itemName${status.index}">${f:h(orderLine.goods.name)}</span></td>
+                    <td><span id="itemPrice${status.index}"><fmt:formatNumber
+                                value="${orderLine.goods.price}" type="CURRENCY"
+                                currencySymbol="&yen;" maxFractionDigits="0" /></span></td>
+                    <td><span id="itemQuantity${status.index}">${f:h(orderLine.quantity)}</span></td>
+                </tr>
+            </c:forEach>
+            <tr>
+                <td>Total</td>
+                <td><span id="totalPrice"><fmt:formatNumber
+                            value="${f:h(order.totalAmount)}" type="CURRENCY"
+                            currencySymbol="&yen;" maxFractionDigits="0" /></span></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <form method="get" action="${pageContext.request.contextPath}/goods">
+            <input type="submit" id="home" value="home" />
+        </form>
+    </div>
+</body>
+</html>
---
+++ session-tutorial-init/session-tutorial-init/session-tutorial-init-web/src/main/java/com/example/session/app/account/AccountCreateController.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2013-2018 NTT DATA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.session.app.account;
+
+import javax.inject.Inject;
+
+import org.dozer.Mapper;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.terasoluna.gfw.common.message.ResultMessages;
+
+import com.example.session.domain.model.Account;
+import com.example.session.domain.service.account.AccountService;
+
+@Controller
+@SessionAttributes(value = { "accountCreateForm" })
+@RequestMapping("account")
+public class AccountCreateController {
+
+	@Inject
+	AccountService accountService;
+
+	@Inject
+	Mapper beanMapper;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+
+	@ModelAttribute(value = "accountCreateForm")
+	public AccountCreateForm setUpAccountForm() {
+		return new AccountCreateForm();
+	}
+
+	@RequestMapping(value = "create", params = "form")
+	public String showCreateForm() {
+		return "account/createForm";
+	}
+
+	@RequestMapping(value = "create", params = "confirm")
+	public String confirmCreate(@Validated AccountCreateForm form,
+			BindingResult result) {
+
+		if (result.hasErrors()) {
+			return showCreateForm();
+		}
+
+		return "account/createConfirm";
+	}
+
+	@RequestMapping(value = "create", params = "redoForm")
+	public String redoCreateForm() {
+		return showCreateForm();
+	}
+
+	@RequestMapping(value = "create", method = RequestMethod.POST)
+	public String update(@Validated AccountCreateForm form, BindingResult result) {
+
+		if (result.hasErrors()) {
+			ResultMessages messages = ResultMessages.error();
+			messages.add("e.st.ac.5001");
+			throw new IllegalOperationException(messages);
+		}
+
+		Account account = beanMapper.map(form, Account.class);
+		accountService.create(account, form.getPassword());
+
+		return "redirect:/account/create?finish";
+	}
+
+	@RequestMapping(value = "create", method = RequestMethod.GET, params = "finish")
+	public String finishCreate() {
+		return "account/createFinish";
+	}
+
+	@RequestMapping(value = "create", method = RequestMethod.GET, params = "home")
+	public String home(SessionStatus sessionStatus) {
+		sessionStatus.setComplete();
+		return "redirect:/goods";
+	}
+
+}
