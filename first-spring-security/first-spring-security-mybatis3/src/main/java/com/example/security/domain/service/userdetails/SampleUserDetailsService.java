acabc367c30e92edc52154c5a99c3110a28248ee
btkatoutmj <katoutmj@pm.nttdata.co.jp>
2018-03-15 15:54:50 +0900

    first-spring-security tutorial

---
+++ first-spring-security/first-spring-security-mybatis3/src/main/java/com/example/security/domain/service/userdetails/SampleUserDetailsService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013-2018 NTT DATA Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.security.domain.service.userdetails;
+
+import javax.inject.Inject;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
+
+import com.example.security.domain.model.Account;
+import com.example.security.domain.service.account.AccountSharedService;
+
+@Service
+public class SampleUserDetailsService implements UserDetailsService { // (1)
+    @Inject
+    AccountSharedService accountSharedService; // (2)
+
+    @Transactional(readOnly=true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            Account account = accountSharedService.findOne(username); // (3)
+            return new SampleUserDetails(account); // (4)
+        } catch (ResourceNotFoundException e) {
+            throw new UsernameNotFoundException("user not found", e); // (5)
+        }
+    }
+
+}
+
