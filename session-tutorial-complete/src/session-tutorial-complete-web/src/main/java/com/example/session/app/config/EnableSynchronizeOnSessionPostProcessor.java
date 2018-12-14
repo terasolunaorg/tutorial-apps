05807a910d0782f96b337c52fe04ed28b38bb1e4
btkobayashirun <kobayashirun@pm.nttdata.co.jp>
2018-12-14 16:25:32 +0900

    fix year of copyright #326 (#328)

--- session-tutorial-complete/src/session-tutorial-complete-web/src/main/java/com/example/session/app/config/EnableSynchronizeOnSessionPostProcessor.java
+++ session-tutorial-complete/src/session-tutorial-complete-web/src/main/java/com/example/session/app/config/EnableSynchronizeOnSessionPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 NTT DATA Corporation. Copyright (C) 2017 NTT Corporation.
+ * Copyright(c) 2013 NTT DATA Corporation. Copyright(c) 2013 NTT Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
