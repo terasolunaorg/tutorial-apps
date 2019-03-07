3f99d04adcaef5d5cde380d1a3553d2c06be0905
tsukamotoknt <tsukamotoknt@pm.nttdata.co.jp>
2019-03-07 19:18:46 +0900

    selenium setting error #336

--- common/scripts/convert-common-test.sh
+++ common/scripts/convert-common-test.sh
@@ -23,6 +23,46 @@ find ./pom.xml | xargs sed -i -e 's|</properties>|    <webdrivermanager.version>
 
 if test `echo $ARCHETYPE_ARTIFACT_ID | grep multi`;then
   # multi project
+  # pom.xml
+  find ./pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-java</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-chrome-driver</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-remote-driver</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-api</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-firefox-driver</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-ie-driver</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-safari-driver</artifactId>\
+                <version>${selenium.version}</version>\
+            </dependency>\
+            <dependency>\
+                <groupId>org.seleniumhq.selenium</groupId>\
+                <artifactId>selenium-support</artifactId>\
+                <version>${selenium.version}</version>\
   # project-selenium/pom.xml
   find ./$ARTIFACT_ID-selenium/pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\
                 <groupId>io.github.bonigarcia</groupId>\
@@ -31,7 +71,58 @@ if test `echo $ARCHETYPE_ARTIFACT_ID | grep multi`;then
             </dependency>\
 \
         </dependencies>|'
+else
+  # single project
+  # pom.xml
+  find ./pom.xml | xargs sed -i -e 's|<artifactId>selenium-java</artifactId>|<artifactId>selenium-java</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-chrome-driver</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-remote-driver</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-api</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-firefox-driver</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-ie-driver</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-safari-driver</artifactId>\
+            <version>${selenium.version}</version>\
+            <scope>test</scope>\
+        </dependency>\
+        <dependency>\
+            <groupId>org.seleniumhq.selenium</groupId>\
+            <artifactId>selenium-support</artifactId>\
+            <version>${selenium.version}</version>|'
 fi
+
+# pom.xml
+find ./pom.xml | xargs sed -i -e 's|</properties>|    <selenium.version>2.46.0</selenium.version>\
+    </properties>|'
 
 if test -n "${TARGET_DIR}/${ARTIFACT_ID}"; then
   popd
