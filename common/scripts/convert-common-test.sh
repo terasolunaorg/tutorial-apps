940b7537efcc65698d0756425b9ec4a60e6a0c8c
bttsukamotoknt <30825972+bttsukamotoknt@users.noreply.github.com>
2019-03-14 15:23:19 +0900

    Add profile #334 (#344)

--- common/scripts/convert-common-test.sh
+++ common/scripts/convert-common-test.sh
@@ -21,6 +21,35 @@ find ./pom.xml | xargs sed -i -e 's|</dependencies>|    <dependency>\
 find ./pom.xml | xargs sed -i -e 's|</properties>|    <webdrivermanager.version>3.1.1</webdrivermanager.version>\
     </properties>|'
 
+find ./pom.xml | xargs sed -i -e 's|</project>|  <profiles>\
+    <profile>\
+      <id>jdk11</id>\
+      <activation>\
+        <jdk>11</jdk>\
+      </activation>\
+      <properties>\
+        <java-version>11</java-version>\
+        <cargo.jvmargs>-Xms512m -Xmx1024m -Djava.locale.providers=COMPAT,CLDR,SPI</cargo.jvmargs>\
+      </properties>\
+      <build>\
+        <pluginManagement>\
+          <plugins>\
+            <plugin>\
+              <groupId>org.apache.maven.plugins</groupId>\
+              <artifactId>maven-surefire-plugin</artifactId>\
+              <configuration>\
+                <systemProperties>\
+                  <jdk.tls.client.protocols>TLSv1.2</jdk.tls.client.protocols>\
+                </systemProperties>\
+              </configuration>\
+            </plugin>\
+          </plugins>\
+        </pluginManagement>\
+      </build>\
+    </profile>\
+  </profiles>\
+</project>|'
+
 if test `echo $ARCHETYPE_ARTIFACT_ID | grep multi`;then
   # multi project
   # project-selenium/pom.xml
