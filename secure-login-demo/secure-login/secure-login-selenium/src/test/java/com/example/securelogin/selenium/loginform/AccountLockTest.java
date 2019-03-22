4af2ee0f550b0bcf5b48ceac78a99c2a0c29e398
btkobayashirun <kobayashirun@pm.nttdata.co.jp>
2019-03-22 15:19:47 +0900

    secure-login tutorial

---
+++ secure-login-demo/secure-login/secure-login-selenium/src/test/java/com/example/securelogin/selenium/loginform/AccountLockTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright(c) 2013 NTT DATA Corporation. Copyright(c) 2013 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.securelogin.selenium.loginform;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jdbc.datasource.init.ScriptException;
+import org.terasoluna.gfw.tutorial.selenium.DBLogFunctionTestSupport;
+import com.example.securelogin.selenium.loginform.page.AbstractPageObject;
+import com.example.securelogin.selenium.loginform.page.login.LoginPage;
+import com.example.securelogin.selenium.loginform.page.passwordchange.PasswordChangePage;
+import com.example.securelogin.selenium.loginform.page.unlock.UnlockSuccessPage;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+
+public class AccountLockTest extends DBLogFunctionTestSupport {
+
+    @Value("${security.lockingThreshold}")
+    int lockingThreshold;
+
+    @Value("${security.lockingDurationSeconds}")
+    int lockingDurationSeconds;
+
+    @Inject
+    @Named("dataSource")
+    DataSource dataSource;
+
+    @Inject
+    ResourceDatabasePopulator populator;
+
+    @Before
+    public void setup() throws ScriptException, SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            populator.populate(connection);
+        }
+    }
+
+    /**
+     * <ul>
+     * <li>test : an account is locked by series of incorrect password attempts
+     * <li>test : account is unlocked when the certain time passes</li>
+     * </ul>
+     */
+    @Test
+    public void testAccountLock001() throws IOException, InterruptedException {
+        AbstractPageObject page = new LoginPage(webDriverOperations, applicationContextUrl)
+                .openWithDescription(
+                        "an account is locked by series of incorrect password attempts"
+                                + " and unlocked when the certain time passes");
+        for (int i = 0; i < lockingThreshold + 1; i++) {
+            page = ((LoginPage) page).loginFailure("demo", "hoge");
+        }
+        assertThat(((LoginPage) page).getLoginError(), is(
+                "User account is locked"));
+
+        webDriverOperations.suspend(lockingDurationSeconds, TimeUnit.SECONDS);
+        page = ((LoginPage) page).loginSuccessIntercepted("demo", "demo");
+        assertTrue(webDriverOperations.getCurrentUrl().endsWith(
+                "/password?form"));
+
+        page = ((PasswordChangePage) page).changePasswordSuccess("demo", "Foo1",
+                "Foo1").gotoTop().logout();
+    }
+
+    /**
+     * <ul>
+     * <li>test : administrator can unlock accounts</li>
+     * </ul>
+     */
+    @Test
+    public void testAccountLock002() throws IOException, InterruptedException {
+        AbstractPageObject page = new LoginPage(webDriverOperations, applicationContextUrl)
+                .openWithDescription("administrator can unlock accounts");
+
+        for (int i = 0; i < lockingThreshold + 1; i++) {
+            page = ((LoginPage) page).loginFailure("demo", "hoge");
+        }
+
+        // unlock
+        page = ((LoginPage) page).loginSuccessIntercepted("admin", "demo")
+                .changePasswordSuccess("demo", "Bar1", "Bar1").gotoTop()
+                .goToUnlockPage().unlockSuccess("demo");
+        assertTrue(webDriverOperations.getCurrentUrl().endsWith(
+                "unlock?complete"));
+
+        // confirm the account is successfully unlocked
+        page = ((UnlockSuccessPage) page).gotoTop().logout()
+                .loginSuccessIntercepted("demo", "demo");
+        assertTrue(webDriverOperations.getCurrentUrl().endsWith(
+                "/password?form"));
+        page = ((PasswordChangePage) page).changePasswordSuccess("demo", "Foo1",
+                "Foo1").gotoTop().logout();
+    }
+
+}
---
+++ secure-login-demo/secure-login/secure-login-web/src/main/webapp/WEB-INF/views/passwordreissue/passwordResetForm.jsp
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Reset Password</title>
+<link rel="stylesheet" href="${f:h(pageContext.request.contextPath)}/resources/app/css/styles.css" />
+</head>
+<body>
+    <div id="wrapper">
+        <h1>Reset Password</h1>
+        <t:messagesPanel />
+        <form:form
+            action="${f:h(pageContext.request.contextPath)}/reissue/resetpassword"
+            method="POST" modelAttribute="passwordResetForm">
+            <table>
+                <tr>
+                    <th><form:label path="username">Username</form:label></th>
+                    <td>${f:h(passwordResetForm.username)} <form:hidden
+                            path="username" value="${f:h(passwordResetorm.username)}" />
+                    </td>
+                    <td></td>
+                </tr>
+                <form:hidden path="token" value="${f:h(passwordResetForm.token)}" />
+                <tr>
+                    <th><form:label path="secret" cssErrorClass="error-label">Secret</form:label>
+                    </th>
+                    <td><form:password path="secret" cssErrorClass="error-input" /></td>
+                    <td><form:errors path="secret" cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <th><form:label path="newPassword" cssErrorClass="error-label">New password</form:label>
+                    </th>
+                    <td><form:password path="newPassword"
+                            cssErrorClass="error-input" /></td>
+                    <td><form:errors path="newPassword" cssClass="error-messages" /></td>
+                </tr>
+                <tr>
+                    <th><form:label path="confirmNewPassword"
+                            cssErrorClass="error-label">New password(Confirm)</form:label></th>
+                    <td><form:password path="confirmNewPassword"
+                            cssErrorClass="error-input" /></td>
+                    <td><form:errors path="confirmNewPassword"
+                            cssClass="error-messages" /></td>
+                </tr>
+            </table>
+
+            <input id="submit" type="submit" value="Reset password" />
+        </form:form>
+    </div>
+</body>
+</html>
