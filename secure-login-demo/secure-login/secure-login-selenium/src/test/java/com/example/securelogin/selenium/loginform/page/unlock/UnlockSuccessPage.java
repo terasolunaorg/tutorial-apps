e1180c4bdbd0fcb92d4bc0965e73f4ecac0379f1
btiwahorisu <iwahorisu@pm.nttdata.co.jp>
2019-06-06 18:27:28 +0900

    secure-login tutorial

---
+++ secure-login-demo/secure-login/secure-login-selenium/src/test/java/com/example/securelogin/selenium/loginform/page/unlock/UnlockSuccessPage.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright(c) 2013 NTT DATA Corporation. Copyright(c) 2013 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.securelogin.selenium.loginform.page.unlock;
+
+import org.terasoluna.gfw.tutorial.selenium.WebDriverOperations;
+import com.example.securelogin.selenium.loginform.page.AuthenticationRequiredPage;
+
+public class UnlockSuccessPage extends AuthenticationRequiredPage {
+
+    {
+        url = "/unlock?complete";
+    }
+
+    public UnlockSuccessPage(WebDriverOperations webDriverOperations,
+            String applicationContextURL) {
+        super(webDriverOperations, applicationContextURL);
+    }
+
+}
