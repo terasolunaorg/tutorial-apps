9367dbdd7855c129147bc829af4d3de0886aaf66
btiwahorisu <40746644+btiwahorisu@users.noreply.github.com>
2020-02-21 12:38:54 +0900

    create interface method #392 (#393)
    * create interface method #392
    
    * exec formatter #392
    
    * add @Transactional #392
    
    * add @Transactional(readOnly = true) #392

--- session-tutorial-init/src/session-tutorial-init-domain/src/main/java/com/example/session/domain/service/account/AccountService.java
+++ session-tutorial-init/src/session-tutorial-init-domain/src/main/java/com/example/session/domain/service/account/AccountService.java
@@ -15,41 +15,13 @@
  */
 package com.example.session.domain.service.account;
 
-import javax.inject.Inject;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
-
 import com.example.session.domain.model.Account;
-import com.example.session.domain.repository.account.AccountRepository;
 
-@Service
-public class AccountService {
+public interface AccountService {
 
-	@Inject
-	AccountRepository accountRespository;
+    public Account findOne(String email);
 
-	@Inject
-	PasswordEncoder passwordEncoder;
+    public void update(Account account);
 
-	public Account findOne(String email) {
-		Account account = accountRespository.findByEmail(email).orElse(null);
-
-		if (account == null) {
-			throw new ResourceNotFoundException("アカウントが存在しません");
-		}
-
-		return account;
-	}
-
-	public void update(Account account) {
-		accountRespository.update(account);
-	}
-
-	public void create(Account account, String password) {
-
-		account.setEncodedPassword(passwordEncoder.encode(password));
-		accountRespository.create(account);
-	}
+    public void create(Account account, String password);
 }
