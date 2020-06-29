0871d9f42db810ebd63716166d92d39d47b40aea
btiwahorisu <iwahorisu@pm.nttdata.co.jp>
2020-06-29 10:54:17 +0900

    session-tutorial

---
+++ session-tutorial-init/session-tutorial-init/session-tutorial-init-web/src/main/java/com/example/session/app/account/IllegalOperationExceptionHandler.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright(c) 2013 NTT DATA Corporation. Copyright(c) 2013 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.session.app.account;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice("session.app.account")
+public class IllegalOperationExceptionHandler {
+
+	@ExceptionHandler({ IllegalOperationException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	ModelAndView handleOrderException(IllegalOperationException e) {
+		return new ModelAndView("common/error/illegalOperationError")
+				.addObject(e.getResultMessages());
+	}
+}
---
+++ session-tutorial-init/session-tutorial-init/session-tutorial-init-web/src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -0,0 +1,3 @@
+<h1>
+    <a href="${pageContext.request.contextPath}/">session-tutorial-init</a>
+</h1>
