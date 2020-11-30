28aaa99d5873004ce13cb4026157adc454793805
btkobayashirun <kobayashirun@pm.nttdata.co.jp>
2020-11-30 19:28:18 +0900

    update 5.6.1.RELEASE

--- todo/scripts/create-app.sh
+++ todo/scripts/create-app.sh
@@ -28,9 +28,9 @@ case "$ARCHETYPE_ARTIFACT_ID" in
         ;;
 esac
 
-export ARCHETYPE_VERSION=${ARCHETYPE_VERSION:=5.6.1-SNAPSHOT}
+export ARCHETYPE_VERSION=${ARCHETYPE_VERSION:=5.6.1.RELEASE}
 export GROUP_ID=com.example.todo
-export VERSION=${VERSION:=1.0.0-SNAPSHOT}
+export VERSION=${VERSION:=5.6.1.RELEASE}
 
 echo "create groupId is ${GROUP_ID}."
 echo "create artifactId is ${ARTIFACT_ID}."
