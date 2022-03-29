bbb32c618d2622bae40df33c1dadd41fcae4f0dd
btootomoyuk <Yuki.Otomo.bp@nttdata.com>
2022-03-29 18:59:08 +0900

    first-spring-security

---
+++ first-spring-security/first-spring-security-mybatis3/src/main/resources/database/H2-dataload.sql
@@ -0,0 +1,3 @@
+INSERT INTO account(username, password, first_name, last_name) VALUES('demo', '{pbkdf2}1dd84f42a7a9a173f8f806d736d34939bed6a36e2948e8bfe88801ee5e6e61b815efc389d03165a4', 'Taro', 'Yamada');
+COMMIT;
+
