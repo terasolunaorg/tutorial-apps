12164ffd9321eaa89a679435b297b76c0d83c5c6
btiwahorisu <iwahorisu@pm.nttdata.co.jp>
2022-04-12 16:55:41 +0900

    todo-rest tutorial

---
+++ todo-rest/todo-api/src/main/java/com/example/todo/domain/service/todo/TodoServiceImpl.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright(c) 2013 NTT DATA Corporation. Copyright(c) 2013 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.todo.domain.service.todo;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.terasoluna.gfw.common.exception.BusinessException;
+import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
+import org.terasoluna.gfw.common.message.ResultMessages;
+
+import com.example.todo.domain.model.Todo;
+import com.example.todo.domain.repository.todo.TodoRepository;
+
+@Service
+@Transactional
+public class TodoServiceImpl implements TodoService {
+
+    private static final long MAX_UNFINISHED_COUNT = 5;
+
+    @Inject
+    TodoRepository todoRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Todo findOne(String todoId) {
+        return todoRepository.findById(todoId).orElseThrow(() -> {
+            ResultMessages messages = ResultMessages.error();
+            messages.add("E404", todoId);
+            return new ResourceNotFoundException(messages);
+        });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Collection<Todo> findAll() {
+        return todoRepository.findAll();
+    }
+
+    @Override
+    public Todo create(Todo todo) {
+        long unfinishedCount = todoRepository.countByFinished(false);
+        if (unfinishedCount >= MAX_UNFINISHED_COUNT) {
+            ResultMessages messages = ResultMessages.error();
+            messages.add("E001", MAX_UNFINISHED_COUNT);
+            throw new BusinessException(messages);
+        }
+
+        String todoId = UUID.randomUUID().toString();
+        Date createdAt = new Date();
+
+        todo.setTodoId(todoId);
+        todo.setCreatedAt(createdAt);
+        todo.setFinished(false);
+
+        todoRepository.create(todo);
+        /* REMOVE THIS LINE IF YOU USE JPA
+            todoRepository.save(todo);
+           REMOVE THIS LINE IF YOU USE JPA */
+
+        return todo;
+    }
+
+    @Override
+    public Todo finish(String todoId) {
+        Todo todo = findOne(todoId);
+        if (todo.isFinished()) {
+            ResultMessages messages = ResultMessages.error();
+            messages.add("E002", todoId);
+            throw new BusinessException(messages);
+        }
+        todo.setFinished(true);
+        todoRepository.update(todo);
+        /* REMOVE THIS LINE IF YOU USE JPA
+            todoRepository.save(todo);
+           REMOVE THIS LINE IF YOU USE JPA */
+        return todo;
+    }
+
+    @Override
+    public void delete(String todoId) {
+        Todo todo = findOne(todoId);
+        todoRepository.delete(todo);
+    }
+}
