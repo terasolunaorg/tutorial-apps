883df575ca60a3c0883418380e9a0da1fc14c681
btiwahorisu <iwahorisu@pm.nttdata.co.jp>
2022-04-12 15:47:30 +0900

    session tutorial

---
+++ session-tutorial-complete/session-tutorial-complete/session-tutorial-complete-domain/src/main/java/com/example/session/domain/service/goods/GoodsService.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright(c) 2013 NTT DATA Corporation. Copyright(c) 2013 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.example.session.domain.service.goods;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.session.domain.model.Goods;
+
+public interface GoodsService {
+
+    public Page<Goods> findByCategoryId(Integer categoryId, Pageable pageable);
+
+    public Goods findOne(String goodsId);
+}
