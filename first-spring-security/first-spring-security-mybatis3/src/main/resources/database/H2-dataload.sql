9d2b02db329cf6ea579e3f3f362624e57e0872e5
btiwahorisu <iwahorisu@pm.nttdata.co.jp>
2023-04-13 18:58:22 +0900

    first-spring-security tutorial

---
+++ first-spring-security/first-spring-security-mybatis3/src/main/resources/database/H2-dataload.sql
@@ -0,0 +1,3 @@
+INSERT INTO account(username, password, first_name, last_name) VALUES('demo', '{pbkdf2@SpringSecurity_v5_8}9cccc80b1782715d013a4db1bd33306e53fc534b5052f9b5ff7f50062f3d6df8d4f3395639686016e5eb803639ca1d10', 'Taro', 'Yamada');
+COMMIT;
+
